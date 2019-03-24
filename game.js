354800d470c375f532bd1698c979fe5986de1bb5
Brian Lobo <brianlobo.code@gmail.com>
2019-03-24 13:25:00 -0400

    Game now picks a random color for you to guess (pickRandColor()) and also changes all the colors of the cards to the target color once the correct card has been clicked

--- game.js
+++ game.js
@@ -7,8 +7,42 @@ var colors = [
 	'rgb(255, 0, 255)'
 ];
 
+var backgroundColor = document.body.style.background;
+var rgbDisplay = document.querySelector('h2');
 var cards = document.querySelectorAll('.card');
+var resultDisplay = document.getElementById('result');
+var targetColor = pickRandColor();
+
+rgbDisplay.textContent = targetColor;
 
 for (let i = 0; i < cards.length; i++) {
+	// Sets cards initial color
 	cards[i].style.backgroundColor = colors[i];
+
+	// Listens for clicks on the card
+	cards[i].addEventListener('click', function() {
+		// Get color of clicked card
+		let clickedColor = this.style.backgroundColor;
+
+		// Compare the two
+		if (clickedColor === targetColor) {
+			resultDisplay.textContent = 'Correct!';
+			changeColors(targetColor);
+		}
+		else {
+			this.style.backgroundColor = backgroundColor;
+			resultDisplay.textContent = 'Wrong, try again';
+		}
+	});
+}
+
+function changeColors(color) {
+	for (let i = 0; i < colors.length; i++) {
+		cards[i].style.background = color;
+	}
+}
+
+function pickRandColor() {
+	let random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
