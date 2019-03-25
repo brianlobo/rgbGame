f76423f3f67c32a37c515b27247248b5fc0ad670
Brian Lobo <brianlobo.code@gmail.com>
2019-03-24 22:33:22 -0400

    Added more comments, added a reset button and applied the logic on the js side

--- game.js
+++ game.js
@@ -1,12 +1,28 @@
-var colors = generateRandArrayColors(6);
-
 var backgroundColor = document.body.style.background;
+var h1 = document.querySelector('h1');
 var rgbDisplay = document.querySelector('h2');
 var cards = document.querySelectorAll('.card');
+var resetButton = document.getElementById('reset');
 var resultDisplay = document.getElementById('result');
+var colors = generateRandArrayColors(6);
 var targetColor = pickRandTargetColor();
 
 rgbDisplay.textContent = targetColor;
+
+resetButton.addEventListener('click', function() {
+	resetButton.textContent = 'New Colors';
+	h1.style.backgroundColor = backgroundColor;
+	// Generate new colors
+	colors = generateRandArrayColors(6);
+	// Pick new target color from array
+	targetColor = pickRandTargetColor();
+	// Updates the
+	rgbDisplay.textContent = targetColor;
+	// Change colors of cards
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].style.backgroundColor = colors[i];
+	}
+});
 
 for (let i = 0; i < cards.length; i++) {
 	// Sets cards initial color
@@ -21,6 +37,8 @@ for (let i = 0; i < cards.length; i++) {
 		if (clickedColor === targetColor) {
 			resultDisplay.textContent = 'Correct!';
 			changeColors(targetColor);
+			h1.style.backgroundColor = targetColor;
+			resetButton.textContent = 'Play again?';
 		}
 		else {
 			this.style.backgroundColor = backgroundColor;
@@ -29,17 +47,22 @@ for (let i = 0; i < cards.length; i++) {
 	});
 }
 
+// Changes the colors of all the cards when
+// answered correctly
 function changeColors(color) {
 	for (let i = 0; i < colors.length; i++) {
 		cards[i].style.background = color;
 	}
 }
 
+// Creates random number between 0 - colors.length
+// and returns a random index in the colors array
 function pickRandTargetColor() {
 	let random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
 
+// Creates an array of num random colors
 function generateRandArrayColors(num) {
 	// Make array
 	var array = [];
@@ -51,7 +74,7 @@ function generateRandArrayColors(num) {
 	return array;
 }
 
-// Generates a random RGB color
+// Generates a single random RGB color string
 function generateRandColor() {
 	let RGBstr = 'rgb(';
 	RGBstr += String(Math.floor(Math.random() * 256)); // R
